3f859fd266d4fdd24bcd6e5259739862c9847973
sumin.choi <pajireg@gmail.com>
2024-09-26 19:00:59 +0900

    added a spinner

--- src/components/comment-form.tsx
+++ src/components/comment-form.tsx
@@ -1,50 +1,163 @@
-
 "use client";
-import { createComment } from '@/lib/actions';
 
-export default function CommentForm() {
+import React, { useState, useRef } from "react";
+import { createComment } from "@/lib/actions";
 
-  return (
-          <form className="mb-6" action={createComment}>
-            <div className="grid grid-cols-2 gap-x-4">
-                <div className="relative mb-6">
-                    <label className="flex  items-center mb-2 text-gray-600 text-sm font-medium"> 이름 <svg width="7"
-                            height="7" className="ml-1" viewBox="0 0 7 7" fill="none" xmlns="http://www.w3.org/2000/svg">
-                            <path
-                                d="M3.11222 6.04545L3.20668 3.94744L1.43679 5.08594L0.894886 4.14134L2.77415 3.18182L0.894886 2.2223L1.43679 1.2777L3.20668 2.41619L3.11222 0.318182H4.19105L4.09659 2.41619L5.86648 1.2777L6.40838 2.2223L4.52912 3.18182L6.40838 4.14134L5.86648 5.08594L4.09659 3.94744L4.19105 6.04545H3.11222Z"
-                                fill="#EF4444" />
-                        </svg>
-                    </label>
-                    <input type="text" id="name" name="name"
-                        className="block w-full h-11 px-5 py-2.5 leading-7 text-base font-normal border border-gray-300 rounded-lg rounded-t-lg placeholder-gray-400 focus:outline-none "
-                        placeholder="이름" required />
-                </div>
-                <div className="relative mb-6">
-                    <label className="flex  items-center mb-2 text-gray-600 text-sm font-medium"> 비밀번호 <svg width="7"
-                            height="7" className="ml-1" viewBox="0 0 7 7" fill="none" xmlns="http://www.w3.org/2000/svg">
-                            <path
-                                d="M3.11222 6.04545L3.20668 3.94744L1.43679 5.08594L0.894886 4.14134L2.77415 3.18182L0.894886 2.2223L1.43679 1.2777L3.20668 2.41619L3.11222 0.318182H4.19105L4.09659 2.41619L5.86648 1.2777L6.40838 2.2223L4.52912 3.18182L6.40838 4.14134L5.86648 5.08594L4.09659 3.94744L4.19105 6.04545H3.11222Z"
-                                fill="#EF4444" />
-                        </svg>
-                    </label>
-                    <input type="text" id="password" name="password"
-                        className="block w-full h-11 px-5 py-2.5 leading-7 text-base font-normal border border-gray-300 rounded-lg rounded-t-lg placeholder-gray-400 focus:outline-none "
-                        placeholder="비밀번호" required />
-                </div>
-            </div>
+const CommentForm = () => {
+	const [formData, setFormData] = useState({
+		name: "",
+		password: "",
+		message: "",
+	});
+	const [loading, setLoading] = useState(false);
 
-            <label className="flex items-center mb-2 text-gray-600 text-sm font-medium"> 메시지 </label>
-              <div className="py-2 px-4 mb-4 bg-white rounded-lg rounded-t-lg border border-gray-300">
-                  <textarea id="message" rows={6} name="message"
-                      className="px-0 w-full border-0 focus:ring-0 focus:outline-none"
-                      placeholder="신랑,신부에게 축하메시지를 남겨주세요." required></textarea>
-              </div>
-              <div className="flex justify-end">
-                <button type="submit"
-                  className="inline-flex items-center py-2.5 px-4 font-medium text-center text-white bg-pink-700 rounded-lg focus:ring-4 focus:ring-pink-200 hover:bg-pink-800">
-                  글쓰기
-                </button>
-              </div>
-          </form>
-  )
-}
+	const formRef = useRef<HTMLFormElement>(null);
+
+	const handleChange = (
+		e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>
+	) => {
+		setFormData({
+			...formData,
+			[e.target.name]: e.target.value,
+		});
+	};
+
+	const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+		e.preventDefault();
+		setLoading(true);
+
+		const data = new FormData();
+		data.append("name", formData.name);
+		data.append("password", formData.password);
+		data.append("message", formData.message);
+
+		await createComment(data); // form data를 액션으로 보냄
+
+		// Reset form data
+		if (formRef.current) {
+			formRef.current.reset();
+		}
+		setFormData({
+			name: "",
+			password: "",
+			message: "",
+		});
+		setLoading(false);
+	};
+
+	return (
+		<div>
+			{loading && (
+				<div className="fixed inset-0 flex items-center justify-center bg-gray-800 bg-opacity-75 z-50">
+					<svg
+						className="animate-spin h-10 w-10 text-white"
+						xmlns="http://www.w3.org/2000/svg"
+						fill="none"
+						viewBox="0 0 24 24"
+					>
+						<circle
+							className="opacity-25"
+							cx="12"
+							cy="12"
+							r="10"
+							stroke="currentColor"
+							strokeWidth="4"
+						></circle>
+						<path
+							className="opacity-75"
+							fill="currentColor"
+							d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4z"
+						></path>
+					</svg>
+				</div>
+			)}
+			<form ref={formRef} className="mb-6" onSubmit={handleSubmit}>
+				<div className="grid grid-cols-2 gap-x-4">
+					<div className="relative mb-6">
+						<label className="flex items-center mb-2 text-gray-600 text-sm font-medium">
+							이름
+							<svg
+								width="7"
+								height="7"
+								className="ml-1"
+								viewBox="0 0 7 7"
+								fill="none"
+								xmlns="http://www.w3.org/2000/svg"
+							>
+								<path
+									d="M3.11222 6.04545L3.20668 3.94744L1.43679 5.08594L0.894886 4.14134L2.77415 3.18182L0.894886 2.2223L1.43679 1.2777L3.20668 2.41619L3.11222 0.318182H4.19105L4.09659 2.41619L5.86648 1.2777L6.40838 2.2223L4.52912 3.18182L6.40838 4.14134L5.86648 5.08594L4.09659 3.94744L4.19105 6.04545H3.11222Z"
+									fill="#EF4444"
+								/>
+							</svg>
+						</label>
+						<input
+							type="text"
+							id="name"
+							name="name"
+							value={formData.name}
+							onChange={handleChange}
+							className="block w-full h-11 px-5 py-2.5 leading-7 text-base font-normal border border-gray-300 rounded-lg rounded-t-lg placeholder-gray-400 focus:outline-none"
+							placeholder="이름"
+							required
+						/>
+					</div>
+					<div className="relative mb-6">
+						<label className="flex items-center mb-2 text-gray-600 text-sm font-medium">
+							비밀번호
+							<svg
+								width="7"
+								height="7"
+								className="ml-1"
+								viewBox="0 0 7 7"
+								fill="none"
+								xmlns="http://www.w3.org/2000/svg"
+							>
+								<path
+									d="M3.11222 6.04545L3.20668 3.94744L1.43679 5.08594L0.894886 4.14134L2.77415 3.18182L0.894886 2.2223L1.43679 1.2777L3.20668 2.41619L3.11222 0.318182H4.19105L4.09659 2.41619L5.86648 1.2777L6.40838 2.2223L4.52912 3.18182L6.40838 4.14134L5.86648 5.08594L4.09659 3.94744L4.19105 6.04545H3.11222Z"
+									fill="#EF4444"
+								/>
+							</svg>
+						</label>
+						<input
+							type="text"
+							id="password"
+							name="password"
+							value={formData.password}
+							onChange={handleChange}
+							className="block w-full h-11 px-5 py-2.5 leading-7 text-base font-normal border border-gray-300 rounded-lg rounded-t-lg placeholder-gray-400 focus:outline-none"
+							placeholder="비밀번호"
+							required
+						/>
+					</div>
+				</div>
+
+				<label className="flex items-center mb-2 text-gray-600 text-sm font-medium">
+					메시지
+				</label>
+				<div className="py-2 px-4 mb-4 bg-white rounded-lg rounded-t-lg border border-gray-300">
+					<textarea
+						id="message"
+						rows={6}
+						name="message"
+						value={formData.message}
+						onChange={handleChange}
+						className="px-0 w-full border-0 focus:ring-0 focus:outline-none"
+						placeholder="신랑,신부에게 축하메시지를 남겨주세요."
+						required
+					></textarea>
+				</div>
+				<div className="flex justify-end">
+					<button
+						type="submit"
+						className="inline-flex items-center py-2.5 px-4 font-medium text-center text-white bg-pink-700 rounded-lg focus:ring-4 focus:ring-pink-200 hover:bg-pink-800"
+						disabled={loading}
+					>
+						글쓰기
+					</button>
+				</div>
+			</form>
+		</div>
+	);
+};
+
+export default CommentForm;
--- src/components/ui/modal.tsx
+++ src/components/ui/modal.tsx
@@ -13,11 +13,14 @@ interface SmallModalProps {
 
 const SmallModal: React.FC<SmallModalProps> = ({ commentId, password, onClose }) => {
   const [inputValue, setInputValue] = useState('');
-  const [open, setOpen] = useState(true)
+  const [open, setOpen] = useState(true);
+  const [loading, setLoading] = useState(false);
 
   const handleSubmit = async () => {
     if (password === inputValue) {
+      setLoading(true);
       await deleteComment(commentId);
+      setLoading(false);
       setOpen(false);
       onClose();
     } else {
@@ -26,62 +29,96 @@ const SmallModal: React.FC<SmallModalProps> = ({ commentId, password, onClose })
   };
 
   return (
-    <Dialog open={open} onClose={() => { setOpen(false); onClose(); }} className="fixed inset-0 z-10 overflow-y-auto">
-      <DialogBackdrop className="fixed inset-0 bg-gray-600 bg-opacity-15 transition-opacity" />
-      <div className="flex min-h-full items-center justify-center p-4 text-center sm:items-center sm:p-0">
-        <DialogPanel
-          transition
-          className="relative transform overflow-hidden rounded-lg bg-white text-left shadow-xl transition-all data-[closed]:translate-y-4 data-[closed]:opacity-0 data-[enter]:duration-300 data-[leave]:duration-200 data-[enter]:ease-out data-[leave]:ease-in sm:my-8 sm:w-full sm:max-w-lg data-[closed]:sm:translate-y-0 data-[closed]:sm:scale-95"
-        >
-          <div className="bg-white px-4 pt-5 sm:p-6 sm:pb-4">
-            <div className="sm:flex sm:items-start">
-              <div className="mx-auto flex h-12 w-12 flex-shrink-0 items-center justify-center rounded-full bg-red-100 sm:mx-0 sm:h-10 sm:w-10">
-                <ExclamationTriangleIcon aria-hidden="true" className="h-6 w-6 text-red-600" />
-              </div>
-              <div className="mt-3 text-center sm:ml-4 sm:mt-0 sm:text-left">
-                <DialogTitle as="h3" className="text-base font-semibold leading-6 text-gray-900">
-                  정말 삭제하시겠습니까?
-                </DialogTitle>
-                <div className="mt-2">
-                <div className="relative my-6">
-                    <label className="flex  items-center mb-2 text-gray-600 text-sm font-medium"> 비밀번호 <svg width="7"
-                            height="7" className="ml-1" viewBox="0 0 7 7" fill="none" xmlns="http://www.w3.org/2000/svg">
-                            <path
-                                d="M3.11222 6.04545L3.20668 3.94744L1.43679 5.08594L0.894886 4.14134L2.77415 3.18182L0.894886 2.2223L1.43679 1.2777L3.20668 2.41619L3.11222 0.318182H4.19105L4.09659 2.41619L5.86648 1.2777L6.40838 2.2223L4.52912 3.18182L6.40838 4.14134L5.86648 5.08594L4.09659 3.94744L4.19105 6.04545H3.11222Z"
-                                fill="#EF4444" />
-                        </svg>
-                    </label>
-                    <input type="text" id="password" name="password" value={inputValue} onChange={(e) => setInputValue(e.target.value)}
-                        className="block w-full h-11 px-5 py-2.5 leading-7 text-base font-normal border border-gray-300 rounded-lg rounded-t-lg placeholder-gray-400 focus:outline-none "
-                        placeholder="비밀번호" required />
+    <>
+{loading && (
+				<div className="fixed inset-0 flex items-center justify-center bg-gray-800 bg-opacity-75 z-100">
+					<svg
+						className="animate-spin h-10 w-10 text-white"
+						xmlns="http://www.w3.org/2000/svg"
+						fill="none"
+						viewBox="0 0 24 24"
+					>
+						<circle
+							className="opacity-25"
+							cx="12"
+							cy="12"
+							r="10"
+							stroke="currentColor"
+							strokeWidth="4"
+						></circle>
+						<path
+							className="opacity-75"
+							fill="currentColor"
+							d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4z"
+						></path>
+					</svg>
+				</div>
+			)}
+      <Dialog open={open} onClose={() => { setOpen(false); onClose(); }} className="fixed inset-0 z-10 overflow-y-auto">
+        <DialogBackdrop className="fixed inset-0 bg-gray-600 bg-opacity-25 transition-opacity" />
+        <div className="flex min-h-full items-center justify-center p-4 text-center sm:items-center sm:p-0">
+          <DialogPanel
+            transition
+            className="relative transform overflow-hidden rounded-lg bg-white text-left shadow-xl transition-all data-[closed]:translate-y-4 data-[closed]:opacity-0 data-[enter]:duration-300 data-[leave]:duration-200 data-[enter]:ease-out data-[leave]:ease-in sm:my-8 sm:w-full sm:max-w-lg data-[closed]:sm:translate-y-0 data-[closed]:sm:scale-95"
+          >
+            <div className="bg-white px-4 pt-5 sm:p-6 sm:pb-4">
+              <div className="sm:flex sm:items-start">
+                <div className="mx-auto flex h-12 w-12 flex-shrink-0 items-center justify-center rounded-full bg-red-100 sm:mx-0 sm:h-10 sm:w-10">
+                  <ExclamationTriangleIcon aria-hidden="true" className="h-6 w-6 text-red-600" />
                 </div>
+                <div className="mt-3 text-center sm:ml-4 sm:mt-0 sm:text-left">
+                  <DialogTitle as="h3" className="text-base font-semibold leading-6 text-gray-900">
+                    정말 삭제하시겠습니까?
+                  </DialogTitle>
+                  <div className="mt-2">
+                    <div className="relative my-6">
+                      <label className="flex items-center mb-2 text-gray-600 text-sm font-medium">
+                        비밀번호
+                        <svg width="7" height="7" className="ml-1" viewBox="0 0 7 7" fill="none" xmlns="http://www.w3.org/2000/svg">
+                          <path
+                            d="M3.11222 6.04545L3.20668 3.94744L1.43679 5.08594L0.894886 4.14134L2.77415 3.18182L0.894886 2.2223L1.43679 1.2777L3.20668 2.41619L3.11222 0.318182H4.19105L4.09659 2.41619L5.86648 1.2777L6.40838 2.2223L4.52912 3.18182L6.40838 4.14134L5.86648 5.08594L4.09659 3.94744L4.19105 6.04545H3.11222Z"
+                            fill="#EF4444"
+                          />
+                        </svg>
+                      </label>
+                      <input
+                        type="text"
+                        id="password"
+                        name="password"
+                        value={inputValue}
+                        onChange={(e) => setInputValue(e.target.value)}
+                        className="block w-full h-11 px-5 py-2.5 leading-7 text-base font-normal border border-gray-300 rounded-lg rounded-t-lg placeholder-gray-400 focus:outline-none"
+                        placeholder="비밀번호"
+                        required
+                      />
+                    </div>
+                  </div>
                 </div>
               </div>
             </div>
-          </div>
-          <div className="bg-gray-50 px-4 py-3 sm:flex sm:flex-row-reverse sm:px-6">
-            <form>
+            <div className="bg-gray-50 px-4 py-3 sm:flex sm:flex-row-reverse sm:px-6">
               <button
                 type="button"
-                onClick={() => { handleSubmit(); }}
+                onClick={handleSubmit}
                 className="inline-flex w-full justify-center rounded-md bg-red-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-red-500 sm:ml-3 sm:w-auto"
+                disabled={loading}
               >
                 삭제
               </button>
-            </form>
-            <button
-              type="button"
-              data-autofocus
-              onClick={() => { setOpen(false); onClose(); }}
-              className="mt-3 inline-flex w-full justify-center rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50 sm:mt-0 sm:w-auto"
-            >
-              취소
-            </button>
-          </div>
-        </DialogPanel>
-      </div>
-    </Dialog>
-  )
+              <button
+                type="button"
+                data-autofocus
+                onClick={() => { setOpen(false); onClose(); }}
+                className="mt-3 inline-flex w-full justify-center rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50 sm:mt-0 sm:w-auto"
+              >
+                취소
+              </button>
+            </div>
+          </DialogPanel>
+        </div>
+      </Dialog>
+    </>
+  );
 }
 
 export default SmallModal;
