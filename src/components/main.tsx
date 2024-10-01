8e8347f9193e3d624c8444dc9f84d065e7c99935
sumin.choi <pajireg@gmail.com>
2024-10-01 21:50:37 +0900

    modified design

--- src/components/main.tsx
+++ src/components/main.tsx
@@ -1,15 +1,19 @@
-import { greatVibes } from '@/app/ui/fonts';
+import Image from "next/image";
 
 export default function Main() {
   return (
-    <div>
-      <div className="relative w-full h-screen bg-cover bg-center" style={{ backgroundImage: `url(/image/main.jpeg)` }}>
-        <div className="flex flex-col justify-start items-center h-screen w-screen pt-8">
-          <p className={`${greatVibes.className} text-gray-400 text-6xl lg:text-8xl`} style={{textShadow: '2px 2px 4px rgba(0, 0, 0, 0.5)'}}>Wedding</p>
-          <br />
-          <p className={`text-gray-400 text-2xl`} style={{textShadow: '2px 2px 4px rgba(0, 0, 0, 0.5)'}}>수민 & 현희</p>
-          <p className="text-gray-400 text-2xl" style={{textShadow: '2px 2px 4px rgba(0, 0, 0, 0.5)'}}>2024년 12월 21일 토요일 오후 2시</p>
-        </div>
+    <div className="flex flex-col h-screen">
+      <div className="relative w-full h-2/4 bg-cover bg-center">
+        <Image src="/image/main.jpeg" alt="Main" width={500} height={500} className="w-full h-full object-cover" />
+      </div>
+      <div className="flex flex-col justify-center items-center h-1/2 bg-white">
+        <br />
+        <p className="text-gray-800 text-2xl mb-4">
+          <span>수 민</span> <span className="font-light text-gray-200 text-xl"> | </span> <span>현 희</span>
+        </p>
+        <br />
+        <p className="text-gray-600">2024년 12월 21일 토요일 오후 2시</p>
+        <p className="text-gray-600">더휴웨딩홀 2층 단독홀</p>
       </div>
     </div>
   );
